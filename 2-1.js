56f8cfe6db42e726d11c934a98f2a6ec9377c9b8
SeungheeLee <1111womyn@naver.com>
2019-09-05 09:45:39 +0900

    second commit

--- 2-1.js
+++ 2-1.js
@@ -6,6 +6,7 @@ var router = require('./2-1-main')(app)
 app.set('views', __dirname+'/views')
 app.set('view engine','ejs')
 app.engine('html', require('ejs').renderFile)
+app.use('/static', express.static(__dirname + '/public'));
 
 app.listen(3000, ()=>{
     console.log("웹 서버가 실행되었다. http://127.0.0.1:3000")
